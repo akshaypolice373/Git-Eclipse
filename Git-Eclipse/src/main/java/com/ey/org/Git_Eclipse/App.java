69ef6421d0b577bc916d56229f191cb26ae083f8
JE385EF <JE385EF@IN3219904W1.ey.net>
2022-05-04 14:05:49 +0530

    First commit

---
+++ Git-Eclipse/src/main/java/com/ey/org/Git_Eclipse/App.java
@@ -0,0 +1,13 @@
+package com.ey.org.Git_Eclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
